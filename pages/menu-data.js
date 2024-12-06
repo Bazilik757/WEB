509a87f586147114d9ffe4ad63de95a4e4a3a9dd
Bazilik757 <159037883+Bazilik757@users.noreply.github.com>
2024-12-06 16:01:06 +0300

    Create menu-data.js
    4 lab

---
+++ pages/menu-data.js
@@ -0,0 +1,85 @@
+const dishes = [
+    {
+        img: "/images/soup/gazpacho.jpg",
+        price: 195,
+        name: "Гаспачо",
+        keyword: "gaspacho",
+        weight: "350г",
+        category: "soup",
+        kind: "veg",
+      },
+      {
+        img: "/images/soup/mushroom_soup.jpg",
+        price: 185,
+        name: "Грибной суп-пюре",
+        keyword: "mushroom",
+        weight: "330г",
+        category: "soup", 
+        kind: "veg",
+      },
+      {
+        img: "/images/soup/norwegian_soup.jpg",
+        price: 270,
+        name: "Норвежский суп",
+        keyword: "norvezskiy",
+        weight: "330г",
+        category: "soup", 
+        kind: "fish",
+      },
+      {
+        img: "/images/main_course/1.jpg",
+        price: 225,
+        name: "Котлеты из курицы с картофельным пюре",
+        keyword: "chicken",
+        weight: "280г",
+        category: "main_dish",
+        kind: "meat" 
+      },
+      {
+        img: "/images/main_course/2.jpg",
+        price: 150,
+        name: "Жареная картошка с грибами",
+        keyword: "friedpotato",
+        weight: "210г",
+        category: "main_dish", 
+        kind: "veg"
+      },
+      {
+        img: "/images/main_course/3.jpg",
+        price: 385,
+        name: "Лазанья",
+        keyword: "lasagna",
+        weight: "310г",
+        category: "main_dish", 
+        kind: "meat"
+      },
+      {
+        img: "/images/beverages/applejuice.jpg",
+        price: 90,
+        name: "Яблочный сок",
+        keyword: "apple_juice",
+        weight: "300мл",
+        category: "juice", 
+        kind: "cold"
+      },
+      {
+        img: "/images/beverages/carrotjuice.jpg",
+        price: 110,
+        name: "Морковный сок",
+        keyword: "carrot_juice",
+        weight: "300мл",
+        category: "juice", 
+        kind: "cold"
+      },
+      {
+        img: "/images/beverages/orangejuice.jpg",
+        price: 120,
+        name: "Апельсиновый сок",
+        keyword: "orange_juice",
+        weight: "300мл",
+        category: "juice", 
+        kind: "cold"
+      }
+];
+
+export default dishes;
